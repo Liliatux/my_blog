f27ff297f8015fa982f6552af909c22f2b7a9648
Lilia <geertszelia@gmail.com>
2017-02-01 16:38:47 +0100

    Etape 3: édit bien un article dans la base de donnée

--- form.php
+++ form.php
@@ -4,13 +4,6 @@
 	ORM::configure('mysql:host=localhost;dbname=my_blog');
 	ORM::configure('username', 'root');
 	ORM::configure('password', 'simplonco');
-
-	$id = $_POST['id'];
-	$posts = ORM::for_table('posts')->find_one($id);
-	$title = $posts->title;
-	$content = $posts->content;
-	$author = $posts->author;
-	$date = $posts->created_at;
 ?>
 
 <!DOCTYPE html>
@@ -21,17 +14,34 @@
 </head>
 <body>
 	<h1>Ajout d'un article</h1>
+	<?php if(empty($_POST['id'])){ ?>
 		<form action="submit_post.php" method="POST">
-			<input type="hidden" name="id" value="<?= $id; ?>">
-			<label for="title">Titre de votre article</label><input type="text" id="title" name="title" value="<?= $title; ?>">
+			<input type="hidden" name="id">
+			<label for="title">Titre de votre article</label><input type="text" id="title" name="title">
 			<br>
-			<label for="content">Contenu</label><textarea name="content" id="content" cols="30" rows="10"><?= $content; ?></textarea>
+			<label for="content">Contenu</label><textarea name="content" id="content" cols="30" rows="10"></textarea>
 			<br>
-			<label for="author">Nom de l'auteur</label><input type="text" id="author" name="author" value="<?= $author; ?>">
+			<label for="author">Nom de l'auteur</label><input type="text" id="author" name="author">
 			<br>
-			<input type="hidden" id="created_at" name="created_at" value="<?=  $date; ?>">
+			<input type="hidden" id="created_at" name="created_at">
 			<br>
 			<input type="submit" value="Envoyer">
 		</form>
+	<?php } else{
+		$posts = ORM::for_table('posts')->find_one($_POST['id']); ?>
+
+		<form action="submit_post.php" method="POST">
+			<input type="hidden" name="id" value="<?= $_POST['id']; ?>">
+			<label for="title">Titre de votre article</label><input type="text" id="title" name="title" value="<?= $posts->title; ?>">
+			<br>
+			<label for="content">Contenu</label><textarea name="content" id="content" cols="30" rows="10"><?= $posts->content; ?></textarea>
+			<br>
+			<label for="author">Nom de l'auteur</label><input type="text" id="author" name="author" value="<?= $posts->author; ?>">
+			<br>
+			<input type="hidden" id="created_at" name="created_at" value="<?= $posts->created_at; ?>">
+			<br>
+			<input type="submit" value="Envoyer">
+		</form>
+	<?php } ?>
 </body>
 </html>
--- submit_post.php
+++ submit_post.php
@@ -1,4 +1,5 @@
 <?php 
+	$id = $_POST['id'];
 	$title = $_POST['title'];
 	$content = $_POST['content'];
 	$author = $_POST['author'];
@@ -7,13 +8,23 @@
 	if(empty($title) || empty($author) || empty($content)){
 		include_once 'form.php';
 		echo "Erreur le titre, l'auteur ou le contenu n'est pas rempli";
-	} else{
+	} 
+
+	if(empty($id)){
 		require_once 'index.php';
 		$posts = ORM::for_table('posts')->create();
 		$posts->title = $title;
 		$posts->content = $content;
 		$posts->author = $author;
-		$posts->created_at = $date;
+		$posts->save();
+		header('Location:index.php');
+		exit;
+	} else{
+		require_once 'index.php';
+		$posts = ORM::for_table('posts')->find_one($id);
+		$posts->title = $title;
+		$posts->content = $content;
+		$posts->author = $author;
 		$posts->save();
 		header('Location:index.php');
 		exit;
